3e48263bfc89efe8630fa798f6e9d05e816343df
Ninhow <allamoolsson09@gmail.com>
2021-02-17 20:29:34 +0100

    Lagt till bootstrap ifall vi behöver komponenterna.. Testat att det fungerar.

--- hackaton2021/apps/hackaton-application/src/app/app.module.ts
+++ hackaton2021/apps/hackaton-application/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    MatSidenavModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
