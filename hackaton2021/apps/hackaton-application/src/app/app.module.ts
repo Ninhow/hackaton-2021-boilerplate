0a47966b3dddafd0cb29ad14d6cacee1d9c66c0b
JohnGNilsson <nilsson.gustav97@gmail.com>
2021-02-19 18:17:52 +0100

    start branch

--- hackaton2021/apps/hackaton-application/src/app/app.module.ts
+++ hackaton2021/apps/hackaton-application/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -15,6 +17,7 @@ import { MatButtonModule } from '@angular/material/button';
     FormsModule,
     MatSidenavModule,
     MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent],
